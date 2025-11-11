bbcb72e3ee6c498cca21286ae0b0435cc7f3b111
seyi.idowu <shezi357412@icloud.com>
2025-11-11 21:00:12 +0100

    feat: Add memoization utility

---
+++ src/utils/memoize.ts
@@ -0,0 +1,15 @@
+export function memoize<Args extends any[], Return>(
+  fn: (...args: Args) => Return
+): (...args: Args) => Return {
+  const cache = new Map<string, Return>();
+  return (...args: Args): Return => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key)!;
+    }
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
+}
+
