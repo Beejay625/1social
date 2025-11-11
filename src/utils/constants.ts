d21b2fffd246859a2a329b7b6c97b7f6397b6b90
seyi.idowu <shezi357412@icloud.com>
2025-11-11 20:58:10 +0100

    feat: Add utility constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const MAX_POST_LENGTH = 10000;
+export const MAX_COMMENT_LENGTH = 5000;
+export const DEFAULT_GAS_LIMIT = 100000n;
+export const DEFAULT_GAS_PRICE = 20000000000n;
+
+export const CONTRACT_EVENTS = {
+  POST_CREATED: 'PostCreated',
+  COMMENT_ADDED: 'CommentAdded',
+  REACTION_ADDED: 'ReactionAdded',
+  PROFILE_UPDATED: 'ProfileUpdated',
+} as const;
+
