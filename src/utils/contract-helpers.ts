b57113130dc06af74d39ac62155771f33803ce64
seyi.idowu <shezi357412@icloud.com>
2025-11-11 20:53:33 +0100

    feat: Add contract helper utilities

---
+++ src/utils/contract-helpers.ts
@@ -0,0 +1,17 @@
+import { SOCIAL_MEDIA_CONTRACT_ADDRESS } from '@/constants/contractAddress';
+
+export function getContractAddress(): string {
+  return SOCIAL_MEDIA_CONTRACT_ADDRESS;
+}
+
+export function isValidContractAddress(address: string): boolean {
+  return /^0x[a-fA-F0-9]{40}$/.test(address);
+}
+
+export function formatContractAddress(address: string): string {
+  if (!isValidContractAddress(address)) {
+    return address;
+  }
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
+
