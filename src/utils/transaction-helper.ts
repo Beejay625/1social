521f87c7c06bbe3df6b9e6e945f679556742130a
seyi.idowu <shezi357412@icloud.com>
2025-11-11 20:55:59 +0100

    feat: Add transaction helper utilities

---
+++ src/utils/transaction-helper.ts
@@ -0,0 +1,17 @@
+export function getTransactionUrl(txHash: string, chainId: number): string {
+  const explorers: Record<number, string> = {
+    1: 'https://etherscan.io/tx/',
+    8453: 'https://basescan.org/tx/',
+    10: 'https://optimistic.etherscan.io/tx/',
+    42161: 'https://arbiscan.io/tx/',
+    137: 'https://polygonscan.com/tx/',
+  };
+  
+  const baseUrl = explorers[chainId] || explorers[1];
+  return `${baseUrl}${txHash}`;
+}
+
+export function isValidTransactionHash(hash: string): boolean {
+  return /^0x[a-fA-F0-9]{64}$/.test(hash);
+}
+
