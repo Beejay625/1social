1b52a8eaa4deb8c75b3dc5561cee3a67aafbe5c0
seyi.idowu <shezi357412@icloud.com>
2025-11-11 20:59:38 +0100

    feat: Add promise utility functions

---
+++ src/utils/promise.ts
@@ -0,0 +1,23 @@
+export function promiseTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
+  return Promise.race([
+    promise,
+    new Promise<T>((_, reject) =>
+      setTimeout(() => reject(new Error('Promise timeout')), ms)
+    ),
+  ]);
+}
+
+export function createPromise<T>(): {
+  promise: Promise<T>;
+  resolve: (value: T) => void;
+  reject: (error: Error) => void;
+} {
+  let resolve: (value: T) => void;
+  let reject: (error: Error) => void;
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve: resolve!, reject: reject! };
+}
+
