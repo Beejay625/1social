13a75818090151562c310eff2d5a06e3719c5c36
seyi.idowu <shezi357412@icloud.com>
2025-11-11 20:54:52 +0100

    feat: Add badge types constants

---
+++ src/constants/badge-types.ts
@@ -0,0 +1,9 @@
+export const BADGE_TYPES = {
+  VERIFIED: 'verified',
+  CREATOR: 'creator',
+  INFLUENCER: 'influencer',
+  BRAND: 'brand',
+} as const;
+
+export type BadgeType = typeof BADGE_TYPES[keyof typeof BADGE_TYPES];
+
