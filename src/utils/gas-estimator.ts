196d887fbcb5218577eaa0e0ca486bf33359f8cd
seyi.idowu <shezi357412@icloud.com>
2025-11-11 20:55:55 +0100

    feat: Add gas estimation utilities

---
+++ src/utils/gas-estimator.ts
@@ -0,0 +1,9 @@
+export function estimateGasCost(gasPrice: bigint, gasLimit: bigint): bigint {
+  return gasPrice * gasLimit;
+}
+
+export function formatGasCost(cost: bigint): string {
+  const eth = Number(cost) / 1e18;
+  return `${eth.toFixed(6)} ETH`;
+}
+
