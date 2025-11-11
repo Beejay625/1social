25985fc964c274adfdba1c76cccad893a7af8427
seyi.idowu <shezi357412@icloud.com>
2025-11-11 21:00:02 +0100

    feat: Add function chaining utilities

---
+++ src/utils/chain.ts
@@ -0,0 +1,8 @@
+export function chainFunctions<T>(...functions: Array<(value: T) => T>): (value: T) => T {
+  return (value: T) => functions.reduce((acc, fn) => fn(acc), value);
+}
+
+export function pipe<T>(value: T, ...functions: Array<(value: T) => T>): T {
+  return chainFunctions(...functions)(value);
+}
+
