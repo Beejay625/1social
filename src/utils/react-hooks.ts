5a76bfcc1835a7c8554af1cc71e22fb22881d1bd
seyi.idowu <shezi357412@icloud.com>
2025-11-11 20:58:37 +0100

    feat: Add React hooks utilities

---
+++ src/utils/react-hooks.ts
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+export function useIsFirstRender(): boolean {
+  const isFirst = useRef(true);
+  if (isFirst.current) {
+    isFirst.current = false;
+    return true;
+  }
+  return false;
+}
+
