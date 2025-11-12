ae5cca201bbf026b0f0c4ab8b6512d82d568bee6
seyi.idowu <shezi357412@icloud.com>
2025-11-12 13:18:52 +0100

    feat: Add staking TypeScript types

---
+++ src/types/staking.ts
@@ -0,0 +1,19 @@
+export interface StakingPool {
+  id: string;
+  creator: string;
+  tokenAddress: string;
+  apy: number;
+  lockPeriod: number;
+  totalStaked: string;
+  participants: number;
+}
+
+export interface Stake {
+  id: string;
+  staker: string;
+  poolId: string;
+  amount: string;
+  timestamp: number;
+  unlockTime: number;
+}
+
