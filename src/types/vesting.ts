4b347ce78a75042fcbdc15424941b06abe3de46e
seyi.idowu <shezi357412@icloud.com>
2025-11-12 13:18:47 +0100

    feat: Add vesting TypeScript types

---
+++ src/types/vesting.ts
@@ -0,0 +1,17 @@
+export interface VestingSchedule {
+  id: string;
+  beneficiary: string;
+  amount: string;
+  startTime: number;
+  cliff: number;
+  duration: number;
+  released: string;
+}
+
+export interface VestingRelease {
+  id: string;
+  scheduleId: string;
+  amount: string;
+  timestamp: number;
+}
+
