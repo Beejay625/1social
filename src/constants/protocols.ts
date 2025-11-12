9d1dd0106e15e5804f9f1efab53eff2fe6f8f322
seyi.idowu <shezi357412@icloud.com>
2025-11-12 13:19:41 +0100

    feat: Add protocols constants

---
+++ src/constants/protocols.ts
@@ -0,0 +1,18 @@
+export const PROTOCOLS = {
+  FARCASTER: 'farcaster',
+  LENS: 'lens',
+  MIRROR: 'mirror',
+  INSTAGRAM: 'instagram',
+  TWITTER: 'twitter',
+} as const;
+
+export type Protocol = typeof PROTOCOLS[keyof typeof PROTOCOLS];
+
+export const PROTOCOL_NAMES: Record<Protocol, string> = {
+  farcaster: 'Farcaster',
+  lens: 'Lens Protocol',
+  mirror: 'Mirror',
+  instagram: 'Instagram',
+  twitter: 'Twitter (X)',
+};
+
