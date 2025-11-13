ad9ba4556b710fb2b233d097273ed2900dc57c82
seyi.idowu <shezi357412@icloud.com>
2025-11-13 21:22:07 +0100

    docs: add JSDoc comment to NFT metadata batch fetcher

--- src/features/onchain/nft-metadata-batch-fetcher.ts
+++ src/features/onchain/nft-metadata-batch-fetcher.ts
@@ -1,5 +1,10 @@
 'use client';
 
+/**
+ * NFT Metadata Batch Fetcher
+ * Fetches metadata for multiple NFTs in batch using Reown wallet
+ */
+
 import { useAccount, useSignMessage } from 'wagmi';
 import { useState } from 'react';
 
